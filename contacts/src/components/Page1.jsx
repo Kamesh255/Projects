70efc4ee7da879c404863a2e5b15e10e1aa0308c
Kamesh255 <kameshhedau19@gmail.com>
2022-05-20 09:15:56 +0530

    update

---
+++ contacts/src/components/Page1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page1 = () => {
+  return (
+    <div >
+        <input type="text" placeholder='Name' />
+        <input type="number" placeholder='Number'/>
+        <input type="submit" />
+    </div>
+  )
+}
+
+export default Page1
